4699ef305e7ecf513d15fc5608dc2e160020329a
Akimatsu98 <he.songchi@gmail.com>
2020-08-20 19:38:04 +0800

    Initial commit

---
+++ src/main/java/UI.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class UI {
+    public static void main(String[] args) {
+        System.out.println("Hello! What can I do for ya? :) \n");
+
+        // code to be added below
+    }
+}
